723cc755eb389aeee56fcbcbb2ecf177aa1963dd
kr4nk <git@jonf.xyz>
2025-06-03 22:44:46 +0200

    did stuff

--- dmenu/sys_clip.sh
+++ dmenu/sys_clip.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-cliphist list | dmenu | cliphist decode  
+#!/bin/fish
+copyq select $(copyq eval -- "tab('clipboard'); for(i=0; i<size(); ++i) print(i +  '| ' + str(read(i)).replace('\n', ' ') + '\n');" | dmenu -b -l 10 -i | cut -d '|' -f 1)
