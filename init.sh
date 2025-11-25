f038c7e2e7e3340718a1d9f46df6a46fbbbab6e2
Jon Jennemann <github@jonf.xyz>
2025-11-25 08:29:56 +0100

    fix

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 cd ~
 
-packages="stow tmux qutebrowser fish ghostty ttf-terminus-nerd ttf-space-mono-nerd ttf-bigblueterminal-nerd python-adblock uv python-adblock ollama nodejs python-pip libffi openssl platformio rtorrent adw-gtk-theme mpv steam "
+packages="stow tmux qutebrowser fish ghostty ttf-terminus-nerd ttf-space-mono-nerd ttf-bigblueterminal-nerd python-adblock uv python-adblock ollama nodejs python-pip libffi openssl platformio rtorrent adw-gtk-theme mpv steam clipse ranger "
 
 
 omarchy-snapshot create
@@ -21,7 +21,17 @@ cp scripts/* ~/scripts/
 yay -Syu --noconfirm
 yay -S $packages --noconfirm
 
-stow .
+###########
+# CHRONOS #
+###########
+git clone https://github.com/samuelstranges/chronos
+cd chronos
+go build .
+mkdir -p ~/.config/chronos/calendars/
+
+########
+# STOW #
+########
 stow --adopt -t ~ fish
 stow --adopt -t ~ ghostty
 stow --adopt -t ~ hypr
@@ -29,6 +39,7 @@ stow --adopt -t ~ nvim
 stow --adopt -t ~ omarchy
 stow --adopt -t ~ qutebrowser
 stow --adopt -t ~ tmux
+stow --adopt -t ~ vesktop
 git reset --hard
 stow -t ~ fish
 stow -t ~ ghostty
@@ -37,12 +48,20 @@ stow -t ~ nvim
 stow -t ~ omarchy
 stow -t ~ qutebrowser
 stow -t ~ tmux
+stow -t ~ vesktop
 
+########
+# TMUX #
+########
 mkdir -p ~/.config/tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 printf "fish" >> ~/.bashrc
 
+
+##########
+# THEMES #
+##########
 curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
 
 cp ~/repos/dotfiles/omarchy/.config/omarchy/hooks/* ~/.config/omarchy/hooks/
